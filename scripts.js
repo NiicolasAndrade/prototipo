f5a30d6fe39d603b7b4cac5e052ab980fc843434
Nícolas <134385017+NiicolasAndrade@users.noreply.github.com>
2024-06-28 13:19:26 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,27 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const form = document.querySelector("footer form");
-
-  form.addEventListener("submit", function (event) {
-    event.preventDefault();
-    alert("Mensagem enviada com sucesso!");
-  });
+document.getElementById('menu-toggle').addEventListener('click', function() {
+  document.querySelector('.sidebar').classList.toggle('active');
 });
+
+const subMenus = document.querySelectorAll('.sub-menu'),
+  buttons = document.querySelectorAll('.sidebar ul button');
+
+const onClick = (item) => {
+  subMenus.forEach((menu) => (menu.style.height = '0px'));
+  buttons.forEach((button) => button.classList.remove('active'));
+
+  if (!item.nextElementSibling) {
+      item.classList.add('active');
+      return;
+  }
+
+  const subMenu = item.nextElementSibling,
+      ul = subMenu.querySelector('ul');
+
+  if (!subMenu.clientHeight) {
+      subMenu.style.height = `${ul.clientHeight}px`;
+      item.classList.add('active');
+  } else {
+      subMenu.style.height = '0px';
+      item.classList.remove('active');
+  }
+};
