4eddb0fb924f80bdf62d2b9cd163a85207c4a624
Nícolas <134385017+NiicolasAndrade@users.noreply.github.com>
2024-07-02 14:40:37 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,27 +1,42 @@
-document.getElementById('menu-toggle').addEventListener('click', function() {
-  document.querySelector('.sidebar').classList.toggle('active');
+document.addEventListener("DOMContentLoaded", function () {
+  const header = document.querySelector("header");
+  const menuToggle = document.getElementById("menu-toggle");
+  const navMenu = document.getElementById("nav-menu");
+
+  // Função para adicionar ou remover a classe sticky ao header
+  function handleScroll() {
+    if (window.scrollY > 50) {
+      header.classList.add("sticky");
+    } else {
+      header.classList.remove("sticky");
+    }
+  }
+
+  // Função para rolar suavemente para o elemento de âncora
+  function handleSmoothScroll(event) {
+    event.preventDefault();
+    const targetId = this.getAttribute("href");
+    document.querySelector(targetId).scrollIntoView({
+      behavior: "smooth",
+    });
+  }
+
+  // Função para alternar a classe ativa no menu
+  function handleMenuToggle() {
+    this.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  }
+
+  // Adiciona o evento de rolagem ao window
+  window.addEventListener("scroll", handleScroll);
+
+  // Adiciona o evento de clique para rolagem suave em links de âncora
+  document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+    anchor.addEventListener("click", handleSmoothScroll);
+  });
+
+  // Adiciona o evento de clique para alternar o menu
+  if (menuToggle && navMenu) {
+    menuToggle.addEventListener("click", handleMenuToggle);
+  }
 });
-
-const subMenus = document.querySelectorAll('.sub-menu'),
-  buttons = document.querySelectorAll('.sidebar ul button');
-
-const onClick = (item) => {
-  subMenus.forEach((menu) => (menu.style.height = '0px'));
-  buttons.forEach((button) => button.classList.remove('active'));
-
-  if (!item.nextElementSibling) {
-      item.classList.add('active');
-      return;
-  }
-
-  const subMenu = item.nextElementSibling,
-      ul = subMenu.querySelector('ul');
-
-  if (!subMenu.clientHeight) {
-      subMenu.style.height = `${ul.clientHeight}px`;
-      item.classList.add('active');
-  } else {
-      subMenu.style.height = '0px';
-      item.classList.remove('active');
-  }
-};
